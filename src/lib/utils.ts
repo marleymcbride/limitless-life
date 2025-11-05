ce53cfb50840d858536b3ab1b04900e74b6b3c5d
marleymcbride <business.marleymcbride@gmail.com>
2025-11-05 11:39:12 +0000

    v4 Hero bacgkround finalized 4.46 - 5/11/25

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -96,7 +96,7 @@ export const unifiedGradientWithSpotlightDesktop = React.createElement("div", {
         rgba(148, 9, 9, 0.0000315) 52%,
         rgba(148, 9, 9, 0.00001575) 53%,
         transparent 55%),
-      linear-gradient(to bottom, transparent 0%, transparent 30%, rgb(0, 0, 0) 30%, rgb(10, 10, 10) 52.5%, #191919 68%, rgba(148, 9, 9, 0.864) 100%)
+      linear-gradient(to bottom, transparent 0%, transparent 30%, rgb(0, 0, 0) 30%, rgb(28, 28, 28) 72%, rgba(148, 9, 9, 0.5) 85%, rgba(148, 9, 9, 0.864) 100%)
     `,
   },
 });
@@ -132,7 +132,7 @@ export const unifiedGradientWithSpotlightMobile = React.createElement("div", {
         rgba(148, 9, 9, 0.0012) 44%,
         rgba(148, 9, 9, 0.0006) 46%,
         transparent 50%),
-      linear-gradient(to bottom, transparent 0%, transparent 30%, rgb(0, 0, 0) 30%, rgb(10, 10, 10) 50%, #191919 60%, rgba(148, 9, 9, 0.35) 78%, rgba(148, 9, 9, 0.39) 82%, rgba(148, 9, 9, 0.5) 86%, rgba(148, 9, 9, 0.68) 91%, rgba(148, 9, 9, 0.864) 95%, rgba(148, 9, 9, 0.864) 100%)
+      linear-gradient(to bottom, transparent 0%, transparent 30%, rgb(0, 0, 0) 30%, rgb(28, 28, 28) 63%, rgba(148, 9, 9, 0.864) 91%)
     `,
   },
 });
