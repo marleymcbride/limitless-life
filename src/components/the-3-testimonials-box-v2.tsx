36ab0450b63450352aabe5d90894bfe512fb33af
marleymcbride <business.marleymcbride@gmail.com>
2026-01-29 17:52:38 +0000

    6.35 The3testimonials positioning tweak - 29/1/26

--- src/components/the-3-testimonials-box-v2.tsx
+++ src/components/the-3-testimonials-box-v2.tsx
@@ -4,9 +4,9 @@ export default function The3TestimonialsBoxV2() {
   return (
       <section className="pt-0 text-white lg:pb-0 mb-0 w-full bg-zinc-400/95">
         <div className="mx-4 md:mx-[3.5%]">
-          <div className="p-10 mx-4 mb-24 mt-20 rounded-2xl shadow-xl lg:mx-0 md:mx-0 lg:max-w-full md:max-w-full bg-zinc-800/95">
-            <div className="grid grid-cols-1 gap-8 md:gap-12 md:grid-cols-3">
-              <div>
+          <div className="p-10 md:p-0 lg:p-0 md:py-10 lg:py-10 mx-4 lg:-px-24 md:-px-24 mb-24 mt-20 rounded-2xl shadow-xl lg:max-w-full md:max-w-full bg-zinc-800/95">
+            <div className="grid grid-cols-1 gap-8 md:gap-12 lg:gap-24 md:grid-cols-3">
+              <div className="text-center md:text-left lg:text-left  lg:-ml-16 md:-ml-16 lg:mr-4 md:-mr-4">
                 <img
                   src="/images/Display photos/luis2.jpeg"
                   alt="Luis"
@@ -18,7 +18,7 @@ export default function The3TestimonialsBoxV2() {
                 <p className="text-lg font-semibold text-zinc-200">— Lewis, Energy Sector</p>
               </div>
 
-              <div>
+              <div className="text-center md:text-left lg:text-left  lg:-ml-4 md:-ml-4 lg:-mr-6 md:-mr-6">
                 <img
                   src="/images/Display photos/aaron.png"
                   alt="Aaron"
@@ -28,7 +28,7 @@ export default function The3TestimonialsBoxV2() {
                 <p className="text-lg font-semibold text-zinc-200">— Aaron, Business Exec</p>
               </div>
 
-              <div>
+              <div className="text-center md:text-left lg:text-left lg:-mr-16 md:-mr-16 lg:ml-6 md:ml-6">
                 <img
                   src="/images/Display photos/laurence.JPG"
                   alt="Laurence"
