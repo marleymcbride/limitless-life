5d6c6f925383d6d786a5bbaccb2d56f0dab6cca0
marleymcbride <business.marleymcbride@gmail.com>
2026-03-04 01:08:00 +0000

    8.1 Beta-tester hero v1 - 3/2/26

---
+++ src/components 2/beta-banner.tsx
@@ -0,0 +1,15 @@
+export default function BetaBanner() {
+  return (
+    <div className="bg-[#940909] py-3 px-4 text-center relative z-50">
+      <p className="text-white hidden md:block lg:block font-normal text-lg md:text-lg lg:text-md tracking-wide font-['-apple-system']">
+      ⚠️ Only <strong>8 spots remaining</strong>. This is the ONLY chance to join the beta cohort.
+      </p>
+
+      <p className="text-white md:hidden lg:hidden font-normal text-lg md:text-lg lg:text-md tracking-wide font-['-apple-system']">
+      ⚠️ Only <strong>8 spots remaining</strong>. This is the ONLY chance to join the beta cohort.
+      </p>
+    </div>
+
+
+  );
+}
